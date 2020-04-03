82524224eaa808876622efaeb790c86e2f3168a8
fl1125 <1245791351@qq.com>
2020-04-03 10:37:28 +0800

    2020.4.3

---
+++ 1、变量与数据类型/HelloWorld.java
@@ -0,0 +1,23 @@
+/*
+   常量分类：
+    A字符串常量   "HelloWorld"
+	B整数常量     "12"
+    C小数常量     "11.12"
+    D字符常量     'a'
+    E布尔常量     true false
+    F空常量       null	
+*/
+public class HelloWorld{
+	public static void main(String[] args){
+		System.out.println("HelloWorld");
+		
+		System.out.println(12);
+		
+		System.out.println(11.12);
+		
+		System.out.println('a');
+		
+		System.out.println(true);
+		System.out.println(false);
+	}
+}
