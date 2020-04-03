85df48d3eaec1517b31f960b1fc6d3e16c9b8e89
fl1125 <1245791351@qq.com>
2020-04-03 16:13:36 +0800

    2020.4.3(2)

---
+++ 1、变量与数据类型/VariableDemo.java
@@ -0,0 +1,42 @@
+/*
+   数据类型： byte,short,int,long,float,double,char,boolean
+   
+
+*/
+public class VariableDemo {
+	public static void main(String[] args){
+	//定义byte类型的变量
+    byte b = 10;
+    System.out.println(10);
+	System.out.println(b);
+	
+	
+	//定义short类型的变量
+	  short s = 100;
+	  System.out.println(s);
+	  
+	//定义int类型的变量
+     int i = 1000;
+      System.out.println(i);
+    
+    //定义long类型的变量（定义long类型的变量时候，最好直接加L）
+	  long l = 10000000000L;
+	  System.out.println(l);
+	
+	//定义float类型的变量
+	  float f = 12.34F;
+	  System.out.println(f);
+	  
+	  //定义doublr类型的变量
+	  double d = 13.33;
+	  System.out.println(d);
+	  
+	  //定义char类型的变量
+	  char c = 'a';
+	  System.out.println(c);
+	  
+	  //定义boolean类型的变量
+	  boolean g = true;
+	  System.out.println(g);
+	}
+}
