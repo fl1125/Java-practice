86bc9b908d61ce6729e493ae40a865ae6f8bb473
fl1125 <1245791351@qq.com>
2020-04-03 11:05:16 +0800

    2020.4.3(1)

---
+++ 1、变量与数据类型/DateType.java
@@ -0,0 +1,30 @@
+/*
+ 数据类型分类：
+  A 基本数据类型（四类八种）
+    1.整数         占用字节数
+      byte           1
+	  shore          2	
+	  int            4
+	  long           8
+	2.浮点数      
+	  float          4
+	  double         8
+	3.字符
+	  char           2
+	4.布尔
+	  boolean        1
+     
+	 PS: 整数默认是int 类型 ，浮点数默认是double类型
+	     定义long类型数据的时候，要加L或者l，建议加L
+		 定义float类型的数据的时候，要加F或者f, 建议加F
+		
+  
+  
+  
+  B 引用数据类型（类 接口 数组）
+*/
+public class Datatype{
+	public static void main(String[] agrs){
+		
+	}
+}
