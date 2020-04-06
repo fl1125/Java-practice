e1952d6a35670977ce1f7e904332b9b36c0d6059
fl1125 <1245791351@qq.com>
2020-04-06 15:48:30 +0800

    2020.4.6

---
+++ workspace/myOperator/src/com/itpractice3/OperatorDemo.java
@@ -0,0 +1,23 @@
+package com.itpractice3;
+/*
+ *  关系运算符：
+ *    ==,!=,>,>=,<,<=
+ *    用来比较两个数据的大小，关系运算符的结果是boolean类型的
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
+public class OperatorDemo {
+
+	public static void main(String[] args) {
+     int a = 10;
+     int b = 20;
+     int c = 10;
+     System.out.println(a == b);
+     System.out.println(a == c);
+	}
+
+}
