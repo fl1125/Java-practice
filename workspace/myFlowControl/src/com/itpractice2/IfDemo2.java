b109bddf90ee61d6a05a4300f153d6f6fadf8370
fl1125 <1245791351@qq.com>
2020-04-08 20:56:43 +0800

    2020.4.8

---
+++ workspace/myFlowControl/src/com/itpractice2/IfDemo2.java
@@ -0,0 +1,25 @@
+package com.itpractice2;
+/*    if语句格式2：
+ *       if(关系表达式){
+ *       语句体1;}
+ *       else{
+ *       语句体2;
+ *       }
+ * 
+ * 
+ * 
+ * 
+ */
+public class IfDemo2 {
+
+	public static void main(String[] args) {
+		int a = 100;
+		if(a%2==0){
+			System.out.println("a是偶数");
+		}
+		else{
+			System.out.println("a是奇数");
+		}
+	}
+
+}
